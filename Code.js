4867a9139a88f99b95da05dff8a9337c794aff7e
KCCHONG1997 <tq121738@gmail.com>
2023-05-20 18:45:34 +0800

    added managerLobby and changed theme

--- Code.js
+++ Code.js
@@ -1,59 +1,196 @@
 function onOpen(e) {
-    SpreadsheetApp.getUi()
-      .createMenu('SAC Manager')
-      .addItem('Make Nominal Role', 'openNominalRoleCreator')
-      .addItem('Make new Event/Session', 'openSessionEventCreator')
-      // .addSeparator()
-      // .addSubMenu(SpreadsheetApp.getUi().createMenu('My Submenu')
-      //     .addItem('One Submenu Item', 'mySecondFunction')
-      //     .addItem('Another Submenu Item', 'myThirdFunction'))
-      .addToUi();
-  }
-  function doGet(e) {
-    return HtmlService.createHtmlOutputFromFile("sessionEventCreator");
-  }
-  function include(filename) {
-    return HtmlService.createHtmlOutputFromFile(filename).setContent();
-  }
-  function moveFiles(sourceFileId, targetFolderId) {
-    var file = DriveApp.getFileById(sourceFileId);
-    var folder = DriveApp.getFolderById(targetFolderId);
-    file.moveTo(folder);
-  }
-  function getSheetById(id) {
-    return SpreadsheetApp.getActive().getSheets().filter(
-      function (s) { return s.getSheetId() === id; }
-    )[0];
-  }
-  
-  
-  function getEventTimeslotLength(googleFormID) {
-    try {
-      var form = FormApp.openById(googleFormID);
-      var formItems = form.getItems();
-      formItems.forEach((item) => {
-        // Logger.log(item.getTitle());
-        if (item.getType() == FormApp.ItemType.MULTIPLE_CHOICE && item.getTitle() == "Select timeline. (If there is only one, just pick the only options)") {
-          Logger.log(item.asMultipleChoiceItem().getChoices()); // return array of choices
-          var choicesArr = item.asMultipleChoiceItem().getChoices();
-          return { choices: choicesArr, status: 'success' }
-        }
-      })
-    } catch (e) {
-      return { msg: "Error: " + e, status : "error"}
+  SpreadsheetApp.getUi()
+    .createMenu('Club Manager')
+    .addItem('Make Nominal Role', 'openNominalRoleCreator')
+    .addItem('Create new Event/Session', 'openSessionEventCreator')
+    .addItem('Manager Lobby', 'openManagerLobby')
+    // .addSeparator()
+    // .addSubMenu(SpreadsheetApp.getUi().createMenu('My Submenu')
+    //     .addItem('One Submenu Item', 'mySecondFunction')
+    //     .addItem('Another Submenu Item', 'myThirdFunction'))
+    .addToUi;
+
+  createOrGetMainFolder();
+}
+
+function createOrGetMainFolder() {
+  var mainFolderName = "ClubManagerDatabase";
+  var subFolderNames = [
+    "ClubManager_EventsGoogleForms",
+    "ClubManager_EventsNominalRole",
+    "ClubManager_DumpedFiles"
+  ];
+  var mainFolder;
+  var folderIdsObject = {}; // Object to store the folder IDs
+
+  // Check if the main folder exists
+  var folders = DriveApp.getFoldersByName(mainFolderName);
+
+  if (folders.hasNext()) {
+    // Folder already exists, retrieve it
+    mainFolder = folders.next();
+
+    // Retrieve existing subfolders
+    var existingSubfolders = mainFolder.getFolders();
+
+    // Loop through the existing subfolders and store their IDs in the object
+    while (existingSubfolders.hasNext()) {
+      var subFolder = existingSubfolders.next();
+      folderIdsObject[subFolder.getName().toUpperCase() + "_FOLDER_ID"] = subFolder.getId();
     }
-  }
-  
-  
-  
-  // Testing Code
-  function listFolderAndFiles() {
-    var folder = DriveApp.getFiles()
-  
-    var i = 0
-    while (folder.hasNext() && i < 10) {
-      Logger.log(folder.next())
-      i = i + 1;
+
+    // Check if the "env.local.variables.txt" file exists in the main folder
+    var files = mainFolder.getFilesByName("env.local.variables.txt");
+    var file;
+    if (files.hasNext()) {
+      // File already exists, retrieve it
+      file = files.next();
+      Logger.log("File 'env.local.variables.txt' already exists in the main folder.");
+    } else {
+      // File does not exist, create it
+      file = mainFolder.createFile("env.local.variables.txt", "");
+      file.setContent(JSON.stringify(folderIdsObject));
+      Logger.log("Created file 'env.local.variables.txt' in the main folder.");
     }
+  } else {
+    // Folder does not exist, create a new one
+    mainFolder = DriveApp.createFolder(mainFolderName);
+
+    // Create subfolders inside the main folder
+    for (var i = 0; i < subFolderNames.length; i++) {
+      var subFolderName = subFolderNames[i];
+      var subFolder = mainFolder.createFolder(subFolderName);
+      folderIdsObject[subFolderName.toUpperCase() + "_FOLDER_ID"] = subFolder.getId(); // Store the subfolder ID in the object
+    }
+
+    // Create the .txt file in the main folder
+    var jsonString = JSON.stringify(folderIdsObject);
+    var file = mainFolder.createFile("env.local.variables.txt", jsonString);
+    Logger.log("Created file 'env.local.variables.txt' in the main folder.");
   }
-  
+
+  // Get the ID of the main folder
+  var mainFolderId = mainFolder.getId();
+
+  // Add the main folder ID to the object
+  folderIdsObject.MAIN_FOLDER_ID = mainFolderId;
+
+  Logger.log("Main folder ID: " + mainFolderId);
+  Logger.log("Subfolder IDs: " + JSON.stringify(folderIdsObject));
+}
+
+
+
+
+
+
+function readFolderIdsFromFile() {
+  var mainFolderName = "ClubManagerDatabase";
+  var mainFolder;
+  var folderIdsObject = {}; // Object to store the folder IDs
+
+  // Check if the main folder exists
+  var folders = DriveApp.getFoldersByName(mainFolderName);
+
+  if (folders.hasNext()) {
+    // Folder already exists, retrieve it
+    mainFolder = folders.next();
+  } else {
+    // Folder does not exist, handle the error condition
+    Logger.log("Main folder not found.");
+    return;
+  }
+
+  // Check if the "env.local.variables.txt" file exists in the main folder
+  var files = mainFolder.getFilesByName("env.local.variables.txt");
+
+  if (files.hasNext()) {
+    // File exists, read its content
+    var file = files.next();
+    var fileContent = file.getBlob().getDataAsString();
+    
+    // Parse the JSON content into an object
+    folderIdsObject = JSON.parse(fileContent);
+
+    // Log the folder IDs
+    Logger.log("Main folder ID: " + folderIdsObject.MAIN_FOLDER_ID);
+    for (var key in folderIdsObject) {
+      if (key !== "MAIN_FOLDER_ID") {
+        Logger.log(key + ": " + folderIdsObject[key]);
+      }
+    }
+  } else {
+    // File does not exist, handle the error condition
+    Logger.log("Folder IDs file not found.");
+  }
+}
+
+
+
+
+
+function doGet(e) {
+  // return HtmlService.createHtmlOutputFromFile("managerLobby");
+  if (!e.parameter.page) {
+    // When no specific page requested, return "home page"
+    return HtmlService.createTemplateFromFile('managerLobby').evaluate();
+  }
+  // else, use page parameter to pick an html file from the script
+  return HtmlService.createTemplateFromFile(e.parameter['page']).evaluate();
+}
+function include(filename) {
+  return HtmlService.createHtmlOutputFromFile(filename).setContent();
+}
+function moveFiles(sourceFileId, targetFolderId) {
+  var file = DriveApp.getFileById(sourceFileId);
+  var folder = DriveApp.getFolderById(targetFolderId);
+  file.moveTo(folder);
+}
+function getSheetById(id) {
+  return SpreadsheetApp.getActive().getSheets().filter(
+    function (s) { return s.getSheetId() === id; }
+  )[0];
+}
+
+
+function getEventTimeslots(googleFormID) {
+  console.log(googleFormID);
+  try {
+    var form = FormApp.openById(googleFormID);
+    var formItems = form.getItems();
+    var returnObj = {}
+    formItems.forEach((item) => {
+      // Logger.log(item.getTitle());
+      if (item.getType() == FormApp.ItemType.MULTIPLE_CHOICE && item.getTitle() == "Select timeline. (If there is only one, just pick the only options)") {
+        // Logger.log(item.asMultipleChoiceItem().getChoices()[0].getValue()); // return array of choices  ---- FIXED
+        var choicesArr_obj = item.asMultipleChoiceItem().getChoices();
+        var choicesArr_text = choicesArr_obj.map(x => x.getValue());
+        returnObj = { arr: choicesArr_text, status: "success" } // NEVER PUT A return inside a forEach Loop
+      } else {
+        returnObj = { msg: "Error: " + "Cannot find the multiple choice question", status: "error" }
+      }
+    })
+    return returnObj
+  } catch (e) {
+    return { msg: "Error: " + e, status: "error" }
+  }
+}
+
+function getScriptUrl() {
+  var url = ScriptApp.getService().getUrl();
+  //  Logger.log(url);
+  return url;
+}
+
+
+
+// Testing Code
+function listFolderAndFiles() {
+  var folder = DriveApp.getFiles()
+
+  var i = 0
+  while (folder.hasNext() && i < 10) {
+    Logger.log(folder.next())
+    i = i + 1;
+  }
+}
--- nominalRoleCreator_gs.js
+++ nominalRoleCreator_gs.js
@@ -1,82 +1,85 @@
 function openNominalRoleCreator() {
-    try {
-      var html = HtmlService.createHtmlOutputFromFile('nominalRoleCreator').setWidth(950).setHeight(750);
-      SpreadsheetApp.getUi().showModalDialog(html, 'Create Nominal Role for an Event');
-    } catch (e) {
-      return { msg: "Error:" + e, status: "error" }
-    }
-  
+  try {
+    var html = HtmlService.createHtmlOutputFromFile('nominalRoleCreator').setWidth(950).setHeight(750);
+    SpreadsheetApp.getUi().showModalDialog(html, 'Create Nominal Role for an Event');
+  } catch (e) {
+    return { msg: "Error:" + e, status: "error" }
   }
-  
-  function getAvailableGoogleForm() {
-    try {
-      var googleFormFolder = DriveApp.getFolderById(googleFormFolderID);
-      var folderList = [];
-  
-      var files = googleFormFolder.getFiles();
-      while (files.hasNext()) {
-        file = files.next();
-        var row = {
-          formID: file.getId(),
-          formName: file.getName(),
-        }
-        folderList.push(row);
+
+}
+
+function getAvailableGoogleForm() {
+  try {
+    var googleFormFolder = DriveApp.getFolderById(googleFormFolderID);
+    var folderList = [];
+
+    var files = googleFormFolder.getFiles();
+    while (files.hasNext()) {
+      file = files.next();
+      var row = {
+        formID: file.getId(),
+        formName: file.getName(),
       }
-      Logger.log(folderList);
-      if (folderList.length > 0) {
-        return folderList
-      } else {
-        return { msg: "Error: No file in directory", status: "error" }
-      }
-    } catch (e) {
-      return { msg: "Error: " + e, status: "error" }
+      folderList.push(row);
     }
-  
-  }
-  
-  function copyNominalRolePrime(eventName, timeslotNum) {
-    try {
-      for (var i = 1; i <= timeslotNum; i++) {
-        var nominalRolePrime = DriveApp.getFileById(nominalRolePrimeID);
-        var folder = DriveApp.getFolderById(nominalRoleFolderID);
-        var newNominalRole = nominalRolePrime.makeCopy(eventName).moveTo(folder);
-      }
-  
-      return { nominalRoleID: newNominalRole.getId(), status: "success" }
-    } catch (e) {
-      return { msg: "Error: " + e, status: "error" }
+    Logger.log(folderList);
+    if (folderList.length > 0) {
+      return folderList
+    } else {
+      return { msg: "Error: No file in directory", status: "error" }
     }
+  } catch (e) {
+    return { msg: "Error: " + e, status: "error" }
   }
-  
-  function createNominalRoleForEvents(eventName, timeslot) {
-  
-  }
-  
-  
-  
-  
-  
-  
-  // Unfinished Function
-  function getRegisteredMemberAdminNumber(formID) {
-    var form = FormApp.openById('12iZuHh51qAmjgjxYEt0vONxkOELTW3exkjoLljHpbVQ');
-    var formResponses = form.getResponses();
-  
-    // get AdminNumber from form. 
-    for (var i = 0; i < formResponses.length; i++) {
-      var adminNumberSignedUp = form.getResponses()[i].getItemResponses()[0].getResponse();
-      Logger.log(adminNumberSignedUp);
+
+}
+
+
+
+
+function copyNominalRolePrime(eventName, timeslots) {
+  try {
+    for (var i = 0; i < timeslots.length; i++) {
+      var nominalRolePrime = DriveApp.getFileById(nominalRolePrimeID);
+      var folder = DriveApp.getFolderById(nominalRoleFolderID);
+      var newNominalRole = nominalRolePrime.makeCopy(eventName + "_" +timeslots[i]).moveTo(folder);
     }
-  
-    // for (var i = 0; i < formResponses.length; i++) {
-    //   var formResponse = formResponses[i];
-    //   var itemResponses = formResponse.getItemResponses();
-    //   for (var j = 0; j < itemResponses.length; j++) {
-    //     var itemResponse = itemResponses[j];
-    //     Logger.log('Response #%s to the question "%s" was "%s"',
-    //       (i + 1).toString(),
-    //       itemResponse.getItem().getTitle(),
-    //       itemResponse.getResponse());
-    //   }
-    // }
+
+    return { nominalRoleID: newNominalRole.getId(), status: "success" }
+  } catch (e) {
+    return { msg: "Error: " + e, status: "error" }
   }
+}
+
+function createNominalRoleForEvents(eventName, timeslot) {
+
+}
+
+
+
+
+
+
+// Unfinished Function
+function getRegisteredMemberAdminNumber(formID) {
+  var form = FormApp.openById('12iZuHh51qAmjgjxYEt0vONxkOELTW3exkjoLljHpbVQ');
+  var formResponses = form.getResponses();
+
+  // get AdminNumber from form. 
+  for (var i = 0; i < formResponses.length; i++) {
+    var adminNumberSignedUp = form.getResponses()[i].getItemResponses()[0].getResponse();
+    Logger.log(adminNumberSignedUp);
+  }
+
+  // for (var i = 0; i < formResponses.length; i++) {
+  //   var formResponse = formResponses[i];
+  //   var itemResponses = formResponse.getItemResponses();
+  //   for (var j = 0; j < itemResponses.length; j++) {
+  //     var itemResponse = itemResponses[j];
+  //     Logger.log('Response #%s to the question "%s" was "%s"',
+  //       (i + 1).toString(),
+  //       itemResponse.getItem().getTitle(),
+  //       itemResponse.getResponse());
+  //   }
+  // }
+}
